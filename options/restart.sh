98cde9674dff7abba996e32e6ab91f852faac57b
Rerechan02 <122332019+Rerechan02@users.noreply.github.com>
2023-07-16 18:26:40 +0700

    Add files via upload

---
+++ options/restart.sh
@@ -0,0 +1,17 @@
+#indo-ssh.com
+#nuralfiyaku
+#FunnyVPN
+#procopas
+#admin-skc
+#Rayzell23
+systemctl restart client
+systemctl restart nginx
+systemctl restart quota
+systemctl restart ws-stunnel
+systemctl restart stunnel5
+systemctl restart xray
+systemctl restart udp-custom
+systemctl restart ssh
+systemctl restart dropbear
+read -n 1 -s -r -p "Press any key to back on menu"
+funny
